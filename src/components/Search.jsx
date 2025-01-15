a725236d3cf43c4312c40a22c22714a94e9e1771
barrera77 <barrera_ml@hotmail.com>
2025-01-15 02:17:50 -0700

    Completed Responsive View (Search, Results & Suggestions)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,17 +5,31 @@ import SearchResults from "./SearchResults";
 
 const Search = () => (
   <div className="recipe-search-wrapper">
-    <div className="flex w-[70%] m-auto justify-center">
+    <div className="sm:flex xs:hidden sm:w-[100%] lg:w-[70%] m-auto justify-center">
       <select name="recipe-category" className="recipe-category p-2 border">
         <option value="0">Select Category</option>
       </select>
       <input
         type="text"
-        className="search-input border w-[50%]"
+        className="search-input border sm:w-[60%] w-[50%]"
         placeholder="Search Recipe. . ."
       />
       <button className="btn-search p-2 bg-indigo-300 text-white">
         <FaSearch />
+      </button>
+    </div>
+    {/* mobile */}
+    <div className="flex flex-col gap-3 xs:w-[100%] justify-center sm:hidden">
+      <select name="recipe-category" className="p-3 border rounded-md">
+        <option value="0">Select Category</option>
+      </select>
+      <input
+        type="text"
+        className="p-[10px] border w-[100%] rounded-md"
+        placeholder="Search Recipe. . ."
+      />
+      <button className="rounded-md p-2 bg-indigo-300 text-white">
+        Search
       </button>
     </div>
     <SearchResults />
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,27 +1,97 @@
 import React from "react";
 import { recipeImage } from "../assets";
+import { FaHeart } from "react-icons/fa";
 
 const SearchResults = () => {
   return (
-    <div className="results-wrapper py-5 mt-5">
-      <div className="pt-5 mb-5 border-b border-indigo-300 ">
+    <div className="results-wrapper py-5 my-5">
+      <div className="pt-5 mb-5 border-b border-indigo-300">
         <h2 className="text-4xl ">Top Results</h2>
       </div>
       <div className="grid-container">
-        <div className="grid grid-flow-col gap-3 overflow-x-auto">
-          <div className="card-container p-[10px] w-[280px] h-[290px]">
-            <div className="recipe-card">
-              <div className="card-image">
-                <img src={recipeImage} alt="" className="squared-image" />
+        <div className="grid xs:grid-flow-row sm:xs:grid-flow-col gap-3 sm:overflow-x-auto xs:overflow-y-auto xs:max-h-[350px]">
+          <div className="recipe-card sm:w-[290px] h-[280px] xs:w-[100%]">
+            <div className="card-image h-[160px] overflow-hidden">
+              <a href="">
+                <img
+                  src={recipeImage}
+                  alt="Recipe Image"
+                  className="squared-image "
+                />
+              </a>
+            </div>
+            <div className="recipe-card-content">
+              <div className="likes-display">
+                <FaHeart className="text-indigo-300 text-xl " />
+                <span>Likes</span>
               </div>
-              <div>Likes</div>
+              <a href="" className="hover:text-indigo-300">
+                Recipe title
+              </a>
+              <div className="text-[14px] my-2">
+                <p>Summary</p>
+              </div>
               <div>
-                <h4>Recipe title</h4>
-                <div>
-                  <p>Summary</p>
+                <div className="text-[12px] text-end">
+                  <span>Prep Time</span>
                 </div>
-                <div>
-                  <div>Prep Time</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="recipe-card sm:w-[290px] h-[280px] xs:w-[100%]">
+            <div className="card-image h-[160px] overflow-hidden">
+              <a href="">
+                <img
+                  src={recipeImage}
+                  alt="Recipe Image"
+                  className="squared-image "
+                />
+              </a>
+            </div>
+            <div className="recipe-card-content">
+              <div className="likes-display">
+                <FaHeart className="text-indigo-300 text-xl " />
+                <span>Likes</span>
+              </div>
+              <a href="" className="hover:text-indigo-300">
+                Recipe title
+              </a>
+              <div className="text-[14px] my-2">
+                <p>Summary</p>
+              </div>
+              <div>
+                <div className="text-[12px] text-end">
+                  <span>Prep Time</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="recipe-card sm:w-[290px] h-[280px] xs:w-[100%]">
+            <div className="card-image h-[160px] overflow-hidden">
+              <a href="">
+                <img
+                  src={recipeImage}
+                  alt="Recipe Image"
+                  className="squared-image "
+                />
+              </a>
+            </div>
+            <div className="recipe-card-content">
+              <div className="likes-display">
+                <FaHeart className="text-indigo-300 text-xl " />
+                <span>Likes</span>
+              </div>
+              <a href="" className="hover:text-indigo-300">
+                Recipe title
+              </a>
+              <div className="text-[14px] my-2">
+                <p>Summary</p>
+              </div>
+              <div>
+                <div className="text-[12px] text-end">
+                  <span>Prep Time</span>
                 </div>
               </div>
             </div>
